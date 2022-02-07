4367be874bf735b0f9926570a50f8b59b4492894
Noah VanSickle <vansickn@union.edu>
2022-02-07 02:50:48 -0500

    main app stuff

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+// import Typer from './components/Typer.vue';
+import TyperGenerator from './components/TyperGenerator.vue';
+</script>
+
+<template>
+  <div class="bg-gray-600 h-screen">
+  <TyperGenerator :random_words="true"/>
+
+  <!--  :custom_word_list="['whats','up','my','dudes','it\'s','wednesday']" -->
+  </div>
+  <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
+
+  <!-- <h2 class="text-3xl">Hello!</h2> -->
+  <!-- <HelloWorld msg="Hello Vue 3 + Vite" /> -->
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  /* text-align: center;
+  color: #2c3e50; */
+  /* margin-top: 60px; */
+}
+</style>
