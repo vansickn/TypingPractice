f8688aa0ba7192c958179875f639c99189689801
Noah VanSickle <vansickn@union.edu>
2022-02-07 02:51:14 -0500

    Typer component

---
+++ src/components/Typer.vue
@@ -0,0 +1,52 @@
+<template>
+{{active_word}}
+<div class="container flex flex-row mx-auto pt-20 items-end">
+    <Word v-for="w in word_list.length" :key="w" :word="word_list[w-1]" :last_word="w == word_list.length"
+     :active="active_word == w-1" @onEndOfWord="nextWord" @toPreviousWord="previousWord"
+     />
+</div>
+
+</template>
+
+<script>
+import Word from '../components/Word.vue';
+// import Word from '../components/Word.vue';
+export default {
+    props: ["word_list"],
+    components: {Word},
+    methods: {
+        nextWord(){
+            this.active_word += 1
+            if(this.active_word > this.word_list.length-1){
+                this.$emit('onEndSentence')
+            }
+        },
+        previousWord(){
+            if(this.active_word > 0){
+                this.active_word -= 1
+            }
+        }
+    },
+    mounted() {
+        console.log(this.word_list)
+    },
+    data() {
+        return {
+            active_word: 0,
+        };
+    },
+    components: { Word },
+    watch: {
+        word_list: function(newv){
+            console.log(newv)
+            if(newv.length != 0){
+                console.log("not null")
+            }
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
