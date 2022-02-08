a7bdd53292b36b9407b38eed524a1b8da4eef438
Noah VanSickle <vansickn@union.edu>
2022-02-08 13:38:33 -0500

    complete refactor of word system

--- src/components/Word.vue
+++ src/components/Word.vue
@@ -1,7 +1,13 @@
 <template>
-   <Letter v-for="n in word.length" :key="n" :letter="word.charAt(n-1)" :active="checkActive(n-1)" :active_word="active" :reset="reset"
+   <!-- <Letter v-for="n in word.length" :key="n" :letter="word.charAt(n-1)" :active="checkActive(n-1)" :active_word="active" :reset="reset"
     @onCorrectPress="onCorrectLetterPress" @onIncorrectPress="onIncorrectLetterPress" @onBackspace="onBackspaceEvent"
-    />
+    /> -->
+
+    <!-- <span class="text-2xl text-gray-500">{{word}}</span>
+    {{typed_letters}} -->
+    <div class="container flex flex-row w-auto">
+        <Letter v-for="n in word.length" :key="n" :letter_array="word.split('')" :typed_letter_array="typed_letters" :index="n-1"/>
+    </div>
    <!-- <span class="text-3xl text-gray-400" :class="{'text-gray-400 text-base':word == '_'}">{{word}}</span> -->
 
   
@@ -14,6 +20,36 @@ export default {
     components: {Letter},
 
     methods: {
+        register_key_press() {
+            if(this.active){
+                document.addEventListener('keydown', this.handleKeyDown);
+                document.addEventListener('keypress', this.handleKeyPress);
+            }
+        },
+        handleKeyDown(e){
+            if(e.keyCode == 8 && this.active){
+                // this.$emit('onBackspace',this.letter)
+                this.handleBackspace();
+            }
+        },
+        handleKeyPress(e){
+            var input = e.key
+            console.log(input)
+            if(e.keyCode == 32){ //space bar
+               console.log("spacebar!") 
+            }else{
+                this.checkActive(input)
+                // this.typed_letters.push(String.fromCharCode(e.keyCode));
+                // console.log(String.fromCharCode(e.keyCode));
+            }
+        },
+        handleBackspace(){
+            if(this.typed_letters.length > 0){
+                this.typed_letters.pop()
+            }else{
+                this.$emit("toPreviousWord");
+            }
+        },
         onCorrectLetterPress(letter){
             // need to set timeout for 1ms in order to switch active
             setTimeout(()=>{
@@ -32,11 +68,9 @@ export default {
                 this.previousLetter()
             })
         },
-        checkActive(n){
-            if(n == this.active_letter){
-                return true
-            }else{
-                return false
+        checkActive(input){
+            if(this.active){
+                this.typed_letters.push(input);
             }
         },
         checkEndWord(){
@@ -61,27 +95,29 @@ export default {
         if(this.active){
             console.log(this.word)
         }
+        this.register_key_press();
     },
     data(){
         return {
-            active_letter: 0
+            active_letter: 0,
+            typed_letters: [],
         }
     },
     watch: {
         active: function(newv,oldv){
-            console.log(newv)
-            console.log(oldv)
-            console.log(this.active_letter)
-            if(oldv == false && newv == true){
-                if(this.active_letter == this.word.length){
-                    this.active_letter = this.word.length-1;
-                    console.log('hello')
-                }
+            if(newv == true){
+                console.log("registering new keypress")
+                this.register_key_press();
             }
         },
         reset: function(newv){
             if(newv){
+                console.log("reset word")
                 this.active_letter = 0;
+                this.typed_letters = [];
+                document.removeEventListener('keydown',this.handleKeyDown);
+                document.removeEventListener('keypress',this.handleKeyPress);
+                this.register_key_press();
             }
         }
     }
