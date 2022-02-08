b0c220eb2c514bac49316d00e321fdc9c0904592
Noah VanSickle <vansickn@union.edu>
2022-02-08 15:04:17 -0500

    words on end of string <3

--- src/components/Letter.vue
+++ src/components/Letter.vue
@@ -1,16 +1,17 @@
 <template>
-  
-
-  <div class="container grid grid-cols-1 w-auto justify-between">
+  <div v-if="!extra_letters" class="container grid grid-cols-1 w-auto justify-between">
     <span class="text-2xl text-gray-500" :class="{'text-gray-200': letter_array[index] == typed_letter_array[index], 'text-red-400': letter_array[index] != typed_letter_array[index] && typed_letter_array.length > index}">{{letter_array[index]}}</span>
-    <span v-if="letter_array[index] != typed_letter_array[index]" class="text-base text-gray-500">{{typed_letter_array[index]}}</span>
+    <span v-if="letter_array[index] != typed_letter_array[index] && typed_letter_array.length <= word_length" class="text-base text-gray-500">{{typed_letter_array[index]}}</span>
+  </div>
+  <div v-if="extra_letters" class="w-auto">
+      <span class="text-2xl text-red-400">{{typed_letter_array[index]}}</span>
   </div>
 
 </template>
 
 <script>
 export default {
-    props: ['letter_array','typed_letter_array','index'],
+    props: ['letter_array','typed_letter_array','index','word_length','typed_letters_length','extra_letters'],
 
     methods: {
 
