aa4e59f23725d594fa4f5b06d11bd2f256f14368
Noah VanSickle <vansickn@union.edu>
2022-02-09 13:42:15 -0500

    error letter a little better

--- src/components/Letter.vue
+++ src/components/Letter.vue
@@ -1,7 +1,7 @@
 <template>
-  <div v-if="!extra_letters" class="container grid grid-cols-1 w-auto justify-between justify-items-center select-none">
+  <div v-if="!extra_letters" class="container grid grid-cols-1 w-auto justify-around justify-items-center select-none">
     <span class="text-3xl text-gray-500" :class="{'text-gray-200': letter_array[index] == typed_letter_array[index], 'text-red-400': letter_array[index] != typed_letter_array[index] && typed_letter_array.length > index, 'text-yellow-300 animate-pulse': index == typed_letter_array.length && active_word}">{{letter_array[index]}}</span>
-    <span v-if="letter_array[index] != typed_letter_array[index] && typed_letter_array.length <= word_length" class="text-xs text-gray-500">{{typed_letter_array[index]}}</span>
+    <span v-if="letter_array[index] != typed_letter_array[index] && typed_letter_array.length <= word_length" class="w-1 text-sm text-gray-500 -mt-2">{{typed_letter_array[index]}}</span>
   </div>
   <div v-if="extra_letters" class="w-auto">
       <span class="text-3xl text-red-400">{{typed_letter_array[index]}}</span>
