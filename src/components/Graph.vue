f40b08d0a32a1878fcbe7b93139d5e39273319a5
Noah VanSickle <vansickn@union.edu>
2022-02-09 19:57:40 -0500

    graph with all info

--- src/components/Graph.vue
+++ src/components/Graph.vue
@@ -1,8 +1,13 @@
 <template>
-        <div class="container flex flex-col min-w-full">
+        <div v-if="showGraph" class="container flex flex-col min-w-full items-center">
+            <h1 class="text-4xl text-gray-500">WPM: {{this.wpm_total}}</h1>
             <div class="container flex flex-row min-w-full justify-center">
                 <!-- <LineChart class="w-10/12" v-bind="barChartProps" /> -->
                 <LineChart v-if="showGraph" class="sm:w-8/12 h-64" :chart-data="chartData" :options="options"/>
+            </div>
+            <div class="container flex flex-row justify-center gap-2">
+               <h1 class="text-lg text-gray-500">Time: {{this.time}}</h1> 
+               <h1 class="text-lg text-gray-500">Accuracy: {{this.correct_words}}/{{this.total_words}}</h1> 
             </div>
         </div>
 
@@ -24,7 +29,7 @@ export default{
         BarChart,
         LineChart,
       },
-    props: ['word_list','wpm_array','correctness_array','generate'], 
+    props: ['word_list','wpm_array','correctness_array','generate','wpm_total','time','correct_words','total_words'], 
     data(){
         return {
             chartData: {
@@ -61,6 +66,7 @@ export default{
                 }
             },
             showGraph: false,
+            wpm_copy: null,
         }
     },
     methods: {
@@ -92,15 +98,17 @@ export default{
     },
     watch: {
         generate: function(newv){
-            if(newv && this.wpm_array != null){
-
-                var wpm_copy = [...this.wpm_array]
-
-                this.generateChartData(this.word_list,wpm_copy);
+            if(this.wpm_array == null){
+                return
+            }
+            var regen = (JSON.stringify(this.wpm_array) == JSON.stringify(this.wpm_copy))
+            if(newv && !regen && this.wpm_array.length == this.total_words){
+                this.wpm_copy = [...this.wpm_array]
+                this.generateChartData(this.word_list,this.wpm_copy);
                 this.showGraph = true;
             }
         }
-    }
+    },
 
 }
 
